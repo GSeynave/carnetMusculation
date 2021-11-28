21bec808632d318e0cb2ccd2af128ba5aa11fe89
Seukachi <54280059+Seukachi@users.noreply.github.com>
2021-11-28 12:20:24 +0100

    init java project

---
+++ carnetMusculation/src/main/java/com/muscu/carnetMusculation/CarnetMusculationApplication.java
@@ -0,0 +1,13 @@
+package com.muscu.carnetMusculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarnetMusculationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarnetMusculationApplication.class, args);
+	}
+
+}
---
+++ carnetMusculation/src/test/java/com/muscu/carnetMusculation/CarnetMusculationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muscu.carnetMusculation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarnetMusculationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
