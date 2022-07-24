c966266d455762e981668690e1d7cf55d966b486
Seukachi <54280059+Seukachi@users.noreply.github.com>
2022-07-24 16:14:55 +0200

    WIP page accueil ...

--- portal/src/app/page/home/home.component.ts
+++ portal/src/app/page/home/home.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { MusculationService } from 'src/app/service/musculation.service';
 
 @Component({
@@ -9,14 +10,28 @@ import { MusculationService } from 'src/app/service/musculation.service';
 })
 export class HomeComponent implements OnInit {
 
-  title ='Demo';
+  title = 'Demo';
   greeting = {};
 
-  constructor(private musculationService: MusculationService, private http: HttpClient) {
+  constructor(
+    private musculationService: MusculationService,
+    private http: HttpClient,
+    private router: Router
+  ) {
     http.get('http://localhost:8080').subscribe(data => this.greeting = data);
-   }
+  }
 
   ngOnInit(): void {
   }
 
+  login() {
+    this.router.navigate(['/login']);
+  }
+
+  isLogin(): boolean {
+    if (sessionStorage.getItem('token') != null && sessionStorage.getItem('token') != '') {
+      return true;
+    }
+    return false;
+  }
 }
